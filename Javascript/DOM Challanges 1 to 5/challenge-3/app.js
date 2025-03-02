702c476e452418e226d00187ebd94d7ad59c1c7f
Ankit Ranjan <112964212+axankit@users.noreply.github.com>
2025-03-02 17:28:24 +0530

    DOM CHALLANGE 3 Completd

--- Javascript/DOM Challanges 1 to 5/challenge-3/app.js
+++ Javascript/DOM Challanges 1 to 5/challenge-3/app.js
@@ -1,3 +1,32 @@
 /**
  * Write your challenge solution here
  */
+
+const nameInput = document.getElementById("nameInput");
+const jobInput = document.getElementById("jobInput");
+const ageInput = document.getElementById("ageInput");
+const bioInput = document.getElementById("bioInput");
+
+// Preview card refernce
+
+const nameDisplay = document.getElementById("nameDisplay");
+const jobDisplay = document.getElementById("jobDisplay");
+const ageDisplay = document.getElementById("ageDisplay");
+const bioDisplay = document.getElementById("bioDisplay");
+
+
+nameInput.addEventListener("input",()=>{
+    nameDisplay.innerText=nameInput.value
+})
+
+jobInput.addEventListener("input",()=>{
+    jobDisplay.innerText=jobInput.value
+})
+
+ageInput.addEventListener("input",()=>{
+    ageDisplay.innerText=ageInput.value
+})
+
+bioInput.addEventListener("input",()=>{
+    bioDisplay.innerText=bioInput.value
+})
