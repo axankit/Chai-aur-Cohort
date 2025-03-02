8dafee98565b61bc75abc5288c5371aa745c7e67
Ankit Ranjan <112964212+axankit@users.noreply.github.com>
2025-03-02 16:54:47 +0530

    DOM JS Challange 2 Completed

--- Javascript/DOM Challanges 1 to 5/challenge-2/app.js
+++ Javascript/DOM Challanges 1 to 5/challenge-2/app.js
@@ -1,3 +1,27 @@
 /**
  * Write your challenge solution here
  */
+
+const mainHeading = document.getElementById("mainHeading");
+
+const redBtn = document.getElementById("redButton");
+const greenBtn = document.getElementById("greenButton");
+const blueBtn = document.getElementById("blueButton");
+const purpleBtn = document.getElementById("purpleButton");
+const resetBtn = document.getElementById("resetButton");
+
+
+
+function changeHeadingColor(color) {
+  mainHeading.style.color = color;
+}
+
+redBtn.addEventListener("click", () => changeHeadingColor("red"));
+
+resetBtn.addEventListener("click", () => changeHeadingColor("black"));
+
+greenBtn.addEventListener("click", () => changeHeadingColor("green"));
+
+blueBtn.addEventListener("click", () => changeHeadingColor("blue"));
+
+purpleBtn.addEventListener("click", () => changeHeadingColor("purple"));
