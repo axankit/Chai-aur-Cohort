b9780493217ec09425bf7327114db258980cd52c
Ankit Ranjan <112964212+axankit@users.noreply.github.com>
2025-03-04 01:37:20 +0530

    DOM Challange Completed

--- Javascript/DOM Challanges 1 to 5/challenge-4/app.js
+++ Javascript/DOM Challanges 1 to 5/challenge-4/app.js
@@ -3,37 +3,72 @@
  */
 
 const taskInput = document.getElementById("taskInput");
-
 const addbtn = document.getElementById("addButton");
-
 const taskList = document.getElementById("taskList");
-
 const totalTasks = document.getElementById("totalTasks");
-
 const completedTasks = document.getElementById("completedTasks");
 
+let count = 0;
+let completedTasksCount = 0;
+
+function taskManagement() {
+  count++;
+  const userInput = taskInput.value;
+
+  //   New element code
+  const li = document.createElement("li");
+  li.innerText = userInput;
+  li.classList.add("task-text");
+  taskList.appendChild(li);
+
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.classList.add("complete-checkbox");
+  li.appendChild(checkbox);
+
+  checkbox.addEventListener("click", () => {
+    li.style.textDecoration = "line-through";
+    li.style.color = "grey";
+    completedTasksCount++;
+    completeCountUpdate(completedTasksCount);
+  });
+
+  //   Delete Button code
+  const delbtn = document.createElement("button");
+  delbtn.innerText = "delete";
+  delbtn.classList.add("delete-button");
+  li.appendChild(delbtn);
+
+  delbtn.addEventListener("click", () => {
+    li.remove();
+
+    count--;
+    countUpdate(count);
+
+    completedTasksCount--;
+    completeCountUpdate(completedTasksCount);
+  });
+
+  // for empty the inputfield
+  taskInput.value = "";
+
+  console.log("Before", count);
+
+  totalTasks.innerText = `Total tasks: ${count}`;
+  completedTasks.innerText = `Completed: ${completedTasksCount}`;
+
+  console.log("After", count);
+}
+
+function countUpdate(count) {
+  totalTasks.innerText = `Total tasks: ${count}`;
+}
+
+function completeCountUpdate(count) {
+  completedTasks.innerText = `Completed: ${count}`;
+}
 
 addbtn.addEventListener("click", () => {
-
-    const li=document.createElement("li")
-    li.innerText=taskInput.value
-    taskList.appendChild(li)
-
-    const delbtn=document.createElement("button")
-    delbtn.innerText="delete"
-    li.appendChild(delbtn)
-
-
-
-    delbtn.addEventListener("click",()=>{
-    taskList.remove(li)
-
-    
-    // for empty the inputfield
-    taskInput.value=""
-})
-
-
-})
-
+  taskManagement();
+});
 
