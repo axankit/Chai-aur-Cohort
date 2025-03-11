08a3e7c903ea17cdf9d3bcb5f12bac28c8917447
Ankit Ranjan <112964212+axankit@users.noreply.github.com>
2025-03-11 14:19:51 +0530

    research on date in  js

--- Javascript/Project 1/research.js
+++ Javascript/Project 1/research.js
@@ -1,15 +1,31 @@
 
 const launchDate = new Date("July 1, 1999, 12:00:00");
 const futureDate = new Date();
-// console.log(futureDate.getTime())
+
 futureDate.setTime(launchDate.getTime());
 
-console.log(futureDate);
+// console.log(futureDate);
 // Expected output: "Thu Jul 01 1999 12:00:00 GMT+0200 (CEST)"
 
-// const fiveMinutesInMillis = 5 * 60 * 1000;
-// futureDate.setTime(futureDate.getTime() + fiveMinutesInMillis);
+const fiveMinutesInMillis = 5 * 60 * 1000;
+futureDate.setTime(futureDate.getTime() + fiveMinutesInMillis);
 
 // console.log(futureDate);
 // Expected output: "Thu Jul 01 1999 12:05:00 GMT+0200 (CEST)"
 // Note: your timezone may vary
+
+
+// Since month is zero based, birthday will be January 10, 1995
+// const birthday = new Date(1994, 11, 10);
+
+const birthday = new Date(2005, 11, 27);
+const copy = new Date();
+
+console.log(copy.getTime());
+
+
+const nex=copy.setTime(birthday.getTime());
+
+
+console.log(nex);
+
