6822e0dacaf4eb6cfc8ace9a4480694cfaa6b266
Ankit Ranjan <112964212+axankit@users.noreply.github.com>
2025-03-21 23:47:17 +0530

    updated

--- Javascript/Kanban Board/Peer Classes/app.js
+++ Javascript/Kanban Board/Peer Classes/app.js
@@ -1,4 +1,5 @@
 let draggedcard = null;
+let rightClickedCard = null;
 
 function addTask(columnId) {
   const input = document.getElementById(`${columnId}-input`);
@@ -24,20 +25,24 @@ function createTaskElement(taskText) {
 
   element.addEventListener("dragstart", dragStart);
   element.addEventListener("dragEnd", dragEnd);
+  element.addEventListener("contextmenu", function(event){
+    event.preventDefault();
+    rightClickedCard = this;
+    showContextMenu(event.pageX, event.pageY);
+  });
   return element;
 }
 // eventhough these are functions why we not use() and why these addevent listner are here
 
 function dragStart() {
-  this.classList.add("draging");
+  this.classList.add("dragging");
   draggedcard = this;
-  document.body.style.background="red"
 }
 
 // why we done this use of this
 
 function dragEnd() {
-  this.classList.remove("draging");
+  this.classList.remove("dragging");
 }
 
 const columns = document.querySelectorAll(".column .tasks");
@@ -52,4 +57,33 @@ columns.forEach((column) => {
 function dragOver(event) {
   event.preventDefault();
   this.appendChild(draggedcard);
+}
+
+// logic of right click edit and delete
+const contextMenu = document.querySelector(".context-menu");
+
+function showContextMenu(x, y) {
+  contextMenu.style.left = `${x}px`;
+  contextMenu.style.top = `${y}px`;
+  contextMenu.style.display = "block";
+}
+
+document.addEventListener("click", () => {
+  contextMenu.style.display = "none";
+});
+
+function editTask(){
+  if (rightClickedCard !== null){
+    const newTaskText=prompt("Edit Task - ", rightClickedCard.textContent)
+
+    if (newTaskText !== ""){
+      rightClickedCard.textContent=newTaskText;
+    }
+  }
+}
+
+function deleteTask(){
+  if (rightClickedCard !== null){
+    rightClickedCard.remove()
+  }
 }
---
+++ Javascript/Kanban Board/Peer Classes/experiment.js
@@ -0,0 +1,19 @@
+// element.addEventListener("contextmenu", function (event) {
+//   rightClickedCard = this;
+// });
+
+
+
+// element.addEventListener("contextmenu", (event) => {
+//   rightClickedCard = this;
+// });
+
+
+class Rectangle {
+    constructor(height, width) {
+      this.height = height;
+      this.width = width;
+    }
+  }
+
+console.log(typeof Rectangle)
