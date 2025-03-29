451b979328f8a6fc7f4a938a76a2d044cc45ba4b
Ankit Ranjan <112964212+axankit@users.noreply.github.com>
2025-03-30 02:13:29 +0530

    authentication updated

--- Full Stack with MangoDB/index.js
+++ Full Stack with MangoDB/index.js
@@ -1,35 +1,28 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 4000;
 
-app.use(cors({
-    origin:"htt"
-}
-))
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Starting me ragdna parta hai')
-})
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/ankit",(req,res)=>{
-    res.send("Mehant karo, jin kandho per jimmedari hoti hai unhe thakna nahi hota hai")
-})
+app.use(
+  cors({
+    origin: "localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-app.get("/avni",(req,res)=>{
-    res.send("Mere pass toh kapre hi nahi hai ")
-})
-
-app.get("/absar",(req,res)=>{
-    res.send("Vichar dhara me bhata hua ")
-})
-
-console.log(process.env.PORT)
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
